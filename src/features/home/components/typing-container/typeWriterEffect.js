c23ab286dcc66cfc0e7801aaf7cb53d33ecaa43d
ERHolguin <rrodshedale@gmail.com>
2024-01-14 22:19:39 +0000

    Move Home-Related Js Code to New Folder Structure

---
+++ src/features/home/components/typing-container/typeWriterEffect.js
@@ -0,0 +1,39 @@
+let subjectSelection = ['Creator.', 'Developer.', 'Polyglot.'];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = subjectSelection[i].split('');
+    let loopTyping = function() {
+        if(word.length > 0) {
+            document.getElementById('word').innerText += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 280);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = subjectSelection[i].split('');
+    let loopDeleting = function() {
+        if(word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerText = word.join(' ');
+        } else {
+            if(subjectSelection.length > (i +1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+};
+
+typingEffect();
