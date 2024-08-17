28d4a45572342b0092460725b9c551a147648e5f
ERHolguin <rrodshedale@gmail.com>
2024-08-17 18:56:00 -0400

    Create Separate JS File for jQuery Common Header Script

---
+++ features/header/common-header.js
@@ -0,0 +1,6 @@
+/*const commonHeader = document.getElementById("header");
+const commonFooter = document.getElementById("footer");*/
+
+$(function loadCommonHeader() {
+  $("#header").load("/features/header/header.html");
+});
