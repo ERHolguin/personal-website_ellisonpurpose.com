5e07c5f06e37c7c37a932b4cb717fcc7e9202c0b
ERHolguin <rrodshedale@gmail.com>
2024-08-23 16:19:28 -0400

    Remove Common Header Due to Security Issues.

--- features/header/common-header.js
+++ features/header/common-header.js
@@ -1,6 +1,7 @@
 /*const commonHeader = document.getElementById("header");
 const commonFooter = document.getElementById("footer");*/
 
+/*
 $(function loadCommonHeader() {
   $("#header").load("/features/header/header.html");
-});
+});*/
